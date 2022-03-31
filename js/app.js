b24389cd32bb2bb0122667fe225ecd235208a09f
marialegil <maria.a.gil@gmail.com>
2022-03-31 00:42:35 -0300

    cajita mensaje

---
+++ js/app.js
@@ -0,0 +1,57 @@
+/*const inputColor= document.querySelector('#inputColor');
+const btnVisualizar= document.querySelector('#btnVisualizar');
+const parrafoExa= document.querySelector('#parrafoExa');
+const cardColor= document.querySelector('#cardColor');
+
+console.log(inputColor.value);
+btnVisualizar.addEventListener("click",()=>{
+    console.log('me diste click');
+    console.log(inputColor.value);
+    parrafoExa.textContent=inputColor.value;
+    cardColor.style.backgroundColor=inputColor.value;
+    btnVisualizar.style.backgroundColor=inputColor.value;
+
+})*/
+
+const carrito = document.getElementById('carrito');
+const template = document.getElementById('template');
+const fragment = document.createDocumentFragment();
+const btnesBotones = document.querySelectorAll('.card .btn');
+
+const carritoObjeto={};
+const agregarAlCarrito = (e) => {
+    console.log(e.target.dataset.fruta);
+    const producto = {
+        
+        titulo: e.target.dataset.fruta,
+        id: e.target.dataset.fruta,
+        cantidad:1
+    };
+       //console.log(producto);
+
+
+    if(carritoObjeto.hasOwnProperty(producto.titulo)){
+        producto.cantidad = carritoObjeto[producto.titulo].cantidad + 1;
+    }
+
+    carritoObjeto[producto.titulo]=producto;
+    pintarCarrito(producto);
+
+    //console.log(producto)
+}
+
+const pintarCarrito=(producto)=>{
+    //console.log("pintar carrito", producto)
+    carrito.textContent='';
+    Object.values(carritoObjeto).forEach((item)=>{
+        const clone = template.content.firstElementChild.cloneNode(true);
+        clone.querySelector('.lead').textContent=item.id;
+        console.log(item.titulo)
+        clone.querySelector('.badge').textContent=item.cantidad;
+        fragment.appendChild(clone);
+    });
+    carrito.appendChild(fragment)
+};
+
+btnesBotones.forEach((btn)=>btn.addEventListener("click",agregarAlCarrito))
+
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Ejemplo carrito de compras con objeto y template
+# template
+```html
+
+<template id="template">
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span class="lead">Frutilla</span>
+        <span class="badge bg-primary rounded-pill">12</span>
+    </li>
+</template>
+
+```
