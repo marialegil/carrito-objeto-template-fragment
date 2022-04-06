1d6d38a4bab969728403e37f71d59a348e1f7124
marialegil <maria.a.gil@gmail.com>
2022-04-06 11:31:48 -0300

    mismo ejemplo pero con array

--- js/app.js
+++ js/app.js
@@ -13,6 +13,8 @@ btnVisualizar.addEventListener("click",()=>{
 
 })*/
 
+
+/*
 const carrito = document.getElementById('carrito');
 const template = document.getElementById('template');
 const fragment = document.createDocumentFragment();
@@ -51,6 +53,66 @@ const pintarCarrito=(producto)=>{
         fragment.appendChild(clone);
     });
     carrito.appendChild(fragment)
+};
+
+btnesBotones.forEach((btn)=>btn.addEventListener("click",agregarAlCarrito))
+
+*/
+
+/*const inputColor= document.querySelector('#inputColor');
+const btnVisualizar= document.querySelector('#btnVisualizar');
+const parrafoExa= document.querySelector('#parrafoExa');
+const cardColor= document.querySelector('#cardColor');
+
+console.log(inputColor.value);
+btnVisualizar.addEventListener("click",()=>{
+    console.log('me diste click');
+    console.log(inputColor.value);
+    parrafoExa.textContent=inputColor.value;
+    cardColor.style.backgroundColor=inputColor.value;
+    btnVisualizar.style.backgroundColor=inputColor.value;
+
+})*/
+
+
+
+const carrito = document.getElementById('carrito');
+const template = document.getElementById('template');
+const fragment = document.createDocumentFragment();
+const btnesBotones = document.querySelectorAll('.card .btn');
+
+const carritoObjeto=[];
+const agregarAlCarrito = (e) => {
+    console.log(e.target.dataset.fruta);
+    const producto = {
+        
+        titulo: e.target.dataset.fruta,
+        id: e.target.dataset.fruta,
+        cantidad:1
+    };
+  
+    const indice=carritoObjeto.findIndex((item) => item.id===producto.id);
+    if(indice === -1){
+        carritoObjeto.push(producto);
+    }else{
+        carritoObjeto[indice].cantidad++;
+    }
+
+    pintarCarrito(carritoObjeto);
+
+}
+
+const pintarCarrito=(array)=>{
+    //console.log("pintar carrito", producto)
+    carrito.textContent='';
+    array.forEach((item)=>{
+        const clone = template.content.firstElementChild.cloneNode(true);
+        clone.querySelector('.lead').textContent=item.id;
+        console.log(item.titulo)
+        clone.querySelector('.badge').textContent=item.cantidad;
+        fragment.appendChild(clone);
+    });
+    carrito.appendChild(fragment);
 };
 
 btnesBotones.forEach((btn)=>btn.addEventListener("click",agregarAlCarrito))
